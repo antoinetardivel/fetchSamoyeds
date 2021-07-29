200f2f29f7616a600d4f35ea5a140c65957345b9
Antoine Tardivel <56165175+antoinetardivel@users.noreply.github.com>
2021-07-29 15:36:00 +0200

    Create samoyeds.js

---
+++ samoyeds.js
@@ -0,0 +1,24 @@
+const getImage = async () => {
+    await fetch('https://www.reddit.com/r/samoyeds.json?sort=top&t=day')
+      .then(response => response.json())
+      //.reddit_video.fallback_url
+      .then(res => {
+        const d = new Date();
+        const hour = d.getHours() + 1;
+        console.log(hour);
+        const number = hour;
+        console.log(res.data.children);
+        if (res.data.children[number].data.media) {
+          console.log(res.data.children[number].data.media.reddit_video.fallback_url);
+        } else if (res.data.children[number].data.url) {
+          console.log(res.data.children[number].data.url);
+        } else if (res.data.children[25].data.media) {
+          console.log(res.data.children[number].data.media.reddit_video.fallback_url);
+        } else if (res.data.children[25].data.url) {
+          console.log(res.data.children[number].data.url);
+        } else {
+          console.log('no file to dispay 🙉');
+        }
+      });
+ };
+getImage();
